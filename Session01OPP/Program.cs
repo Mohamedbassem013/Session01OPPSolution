552bd1e6033ff4bad159dc141959edc8fe4475b4
unknown <Muhammed Bassem@DESKTOP-DA9SS6B>
2024-07-04 01:16:14 +0300

    Add project files.

---
+++ Session01OPP/Program.cs
@@ -0,0 +1,66 @@
+﻿
+
+
+#region excption
+
+#region Example 01
+//static void DoSomeCode()
+//{
+//    int x , y , z;
+//     x = int.Parse(Console.ReadLine());
+//     y = int.Parse(Console.ReadLine());
+
+//    z = x / y;
+//    int[] arr = { 1, 2, 3, };
+//    arr[100] = 22;
+//}
+////entry point
+//DoSomeCode();
+
+#endregion
+
+/////////////////////////////////////
+
+#region Example 02
+//static void DoSomeProtectiveCode()
+//{
+//    int x, y, z;
+//    do
+//    {
+//        Console.WriteLine("Pla Enter the frist number : ");
+
+//    } while (!int.TryParse(Console.ReadLine(), out x));
+
+//    do
+//    {
+//        Console.WriteLine("Pla Enter the Secound number : ");
+
+//    } while (!int.TryParse(Console.ReadLine(), out y) || y == 0);
+
+
+//    z = x / y;
+//    int[] arr = { 1, 2, 3, };
+//    if (arr?.Length > 100)
+//    {
+//        arr[100] = 22;
+//    }
+//}
+////entry point
+//try
+//{
+//    DoSomeProtectiveCode();
+
+//}
+//catch (Exception e)
+//{
+//    Console.WriteLine(e.Message);
+//}
+//finally
+//{
+//    Console.WriteLine("Finally");
+//} 
+#endregion
+
+#endregion
+
+ 
